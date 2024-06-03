9973b4f7c642f1d80bd71c8aab88956c91791282
jmmauricio <jmmauricio6@gmail.com>
2024-06-03 11:35:45 +0200

    vsc now can single phase

--- src/pydae/__init__.py
+++ src/pydae/__init__.py
@@ -7,5 +7,5 @@ Differential Algebraic Equations in Python
 
 """
 
-__version__ = "0.53.2"
+__version__ = "0.53.3"
  
--- src/pydae/urisi/vsc_ctrls/ctrl_3ph_4w_droop.py
+++ src/pydae/urisi/vsc_ctrls/ctrl_3ph_4w_droop.py
@@ -35,14 +35,19 @@ def ctrl_3ph_4w_droop(grid,vsc_data,ctrl_data,name,bus_name):
     v_dc = ((V_ph_r-V_n_r)**2 + (V_ph_i-V_n_i)**2)**0.5
 
     K_acdc = sym.Symbol(f'K_acdc_{bus_ac}', real=True)
+    K_acdc_a = sym.Symbol(f'K_acdc_a_{bus_ac}', real=True)
+    K_acdc_b = sym.Symbol(f'K_acdc_b_{bus_ac}', real=True)
+    K_acdc_c = sym.Symbol(f'K_acdc_c_{bus_ac}', real=True)
+
     p_vsc_a,p_vsc_b,p_vsc_c = sym.symbols(f'p_vsc_a_{bus_ac},p_vsc_b_{bus_ac},p_vsc_c_{bus_ac}',real=True)
+    p_vsc_a_ref,p_vsc_b_ref,p_vsc_c_ref = sym.symbols(f'p_vsc_a_ref_{bus_ac},p_vsc_b_ref_{bus_ac},p_vsc_c_ref_{bus_ac}',real=True)
 
     V_ac_b = U_ac_b/np.sqrt(3)
 
     v_dc_pu = v_dc/800.0
-    p_ac_a = K_acdc*(v_dc_pu - V_phn[0]/V_ac_b)
-    p_ac_b = K_acdc*(v_dc_pu - V_phn[1]/V_ac_b)
-    p_ac_c = K_acdc*(v_dc_pu - V_phn[2]/V_ac_b)
+    p_ac_a = K_acdc*K_acdc_a*(v_dc_pu - V_phn[0]/V_ac_b) + p_vsc_a_ref
+    p_ac_b = K_acdc*K_acdc_b*(v_dc_pu - V_phn[1]/V_ac_b) + p_vsc_b_ref
+    p_ac_c = K_acdc*K_acdc_c*(v_dc_pu - V_phn[2]/V_ac_b) + p_vsc_c_ref
 
     grid.dae['g'] += [p_ac_a - p_vsc_a]
     grid.dae['g'] += [p_ac_b - p_vsc_b]
@@ -57,13 +62,23 @@ def ctrl_3ph_4w_droop(grid,vsc_data,ctrl_data,name,bus_name):
     grid.dae['y_run'] += [p_vsc_c]
 
     grid.dae['params_dict'].update({f'K_acdc_{bus_ac}':0.0})
-
+    grid.dae['params_dict'].update({f'K_acdc_a_{bus_ac}':1.0})
+    grid.dae['params_dict'].update({f'K_acdc_b_{bus_ac}':1.0})
+    grid.dae['params_dict'].update({f'K_acdc_c_{bus_ac}':1.0})
+            
     grid.dae['u_ini_dict'].pop(f'p_vsc_a_{bus_ac}')
     grid.dae['u_ini_dict'].pop(f'p_vsc_b_{bus_ac}')
     grid.dae['u_ini_dict'].pop(f'p_vsc_c_{bus_ac}')
     grid.dae['u_run_dict'].pop(f'p_vsc_a_{bus_ac}')
     grid.dae['u_run_dict'].pop(f'p_vsc_b_{bus_ac}')
     grid.dae['u_run_dict'].pop(f'p_vsc_c_{bus_ac}')
+
+    grid.dae['u_ini_dict'].update({f'p_vsc_a_ref_{bus_ac}':0.0})
+    grid.dae['u_ini_dict'].update({f'p_vsc_b_ref_{bus_ac}':0.0})
+    grid.dae['u_ini_dict'].update({f'p_vsc_c_ref_{bus_ac}':0.0})
+    grid.dae['u_run_dict'].update({f'p_vsc_a_ref_{bus_ac}':0.0})
+    grid.dae['u_run_dict'].update({f'p_vsc_b_ref_{bus_ac}':0.0})
+    grid.dae['u_run_dict'].update({f'p_vsc_c_ref_{bus_ac}':0.0})
 
     grid.dae['h_dict'].update({f'v_ac_a_pu_{bus_ac}':V_phn[0]/V_ac_b})
     grid.dae['h_dict'].update({f'v_ac_b_pu_{bus_ac}':V_phn[1]/V_ac_b})
